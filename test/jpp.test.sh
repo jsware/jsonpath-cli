fe540f048a4d3c7b8b1c56d33023936bad3b4ee9
John Scott <68079471+JohnScottUK@users.noreply.github.com>
2021-09-23 23:20:43 +0100

    Added tests

---
+++ test/jpp.test.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+test="$(dirname $0)"
+jpp="$test/../bin/jpp"
+
+testBasic() {
+  result=$($jpp "$test/openapi.json" '$'|wc -l)
+  assertEquals $result 1
+}
+
+testPretty() {
+  count=$(cat $test/openapi.json|wc -l)
+  result=$($jpp --pretty "$test/openapi.json" '$'|wc -l)
+  assertEquals $result $((count +2))
+}
+
+testSeparate() {
+  result=$($jpp --separate --jsonpath '$.paths[?(@.get && @.get.parameters)]~' $test/openapi.json|wc -l)
+
+  assertEquals $result 7
+}
+
+# Load shUnit2.
+. $test/shunit2/shunit2
